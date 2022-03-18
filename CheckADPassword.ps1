b4731a9eacf3fec5add05e85c3473693d6c5cfed
Runard <40338703+alexandremottier@users.noreply.github.com>
2022-03-18 13:48:10 +0100

    v1.1
    v1.1

---
+++ CheckADPassword.ps1
@@ -0,0 +1,22 @@
+﻿While(0 -lt 1)
+  {
+  Function Test-ADAuthentication {
+      param($username,$password)
+      (new-object directoryservices.directoryentry "",$username,$password).psbase.name -ne $null
+  }
+  Write-Host -ForegroundColor Yellow "...::: Outil de vérification de mot de passe Active Directory :::..."
+  $login = Read-Host "Quel est l'identifiant de la session ?"
+  $password = Read-Host "Quel est le mot de passe de la session ?"-AsSecureString
+
+  $password = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+  $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($password)
+
+  $result = Test-ADAuthentication "$login" "$password"
+
+  If($result -eq "True")
+    {
+        Write-Host -ForegroundColor Green "Le mot de passe AD est correct !"
+    }else{
+        Write-Host -ForegroundColor Red "Le mot de passe AD est incorrect."
+    }
+}
